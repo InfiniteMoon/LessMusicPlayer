ca3e9793bccbfadd98360419074477216ed09909
吴星宇 <102565297+InfiniteMoon@users.noreply.github.com>
2022-10-07 15:22:51 +0800

    Add files via upload

---
+++ LessMusicPlayer/LessMusicPlayer/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace LessMusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.listBoxMusic = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPrior = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lblMusicName = new System.Windows.Forms.Label();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.16346F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.83654F));
+            this.tableLayoutPanel1.Controls.Add(this.trackBar1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.listBoxMusic, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.lblMusicName, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnInput, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.axWindowsMediaPlayer1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 9F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.11449F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.545617F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.57424F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.781362F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(420, 593);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trackBar1.Location = new System.Drawing.Point(117, 37);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(298, 25);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseDown);
+            this.trackBar1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseUp);
+            // 
+            // listBoxMusic
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.listBoxMusic, 2);
+            this.listBoxMusic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxMusic.Font = new System.Drawing.Font("微软雅黑", 10.28571F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBoxMusic.FormattingEnabled = true;
+            this.listBoxMusic.HorizontalScrollbar = true;
+            this.listBoxMusic.ItemHeight = 23;
+            this.listBoxMusic.Location = new System.Drawing.Point(5, 68);
+            this.listBoxMusic.Name = "listBoxMusic";
+            this.listBoxMusic.ScrollAlwaysVisible = true;
+            this.listBoxMusic.Size = new System.Drawing.Size(410, 450);
+            this.listBoxMusic.TabIndex = 3;
+            this.listBoxMusic.SelectedIndexChanged += new System.EventHandler(this.listBoxMusic_SelectedIndexChanged);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.SystemColors.Control;
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 2);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.btnPlay, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnPause, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnStop, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnPrior, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnNext, 4, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(5, 524);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(410, 43);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnPlay.Font = new System.Drawing.Font("微软雅黑", 9.07563F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnPlay.Location = new System.Drawing.Point(3, 3);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(76, 37);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "播放";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnPause.Font = new System.Drawing.Font("微软雅黑", 9.07563F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnPause.Location = new System.Drawing.Point(85, 3);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(76, 37);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.Text = "暂停";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnStop.Font = new System.Drawing.Font("微软雅黑", 9.07563F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnStop.Location = new System.Drawing.Point(167, 3);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(76, 37);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPrior
+            // 
+            this.btnPrior.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnPrior.Font = new System.Drawing.Font("微软雅黑", 9.07563F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnPrior.Location = new System.Drawing.Point(249, 3);
+            this.btnPrior.Name = "btnPrior";
+            this.btnPrior.Size = new System.Drawing.Size(76, 37);
+            this.btnPrior.TabIndex = 3;
+            this.btnPrior.Text = "上一首";
+            this.btnPrior.UseVisualStyleBackColor = true;
+            this.btnPrior.Click += new System.EventHandler(this.btnPrior_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnNext.Font = new System.Drawing.Font("微软雅黑", 9.07563F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNext.Location = new System.Drawing.Point(331, 3);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(76, 37);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "下一首";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lblMusicName
+            // 
+            this.lblMusicName.AutoSize = true;
+            this.lblMusicName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMusicName.Font = new System.Drawing.Font("微软雅黑", 9.07563F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblMusicName.Location = new System.Drawing.Point(115, 12);
+            this.lblMusicName.Margin = new System.Windows.Forms.Padding(1);
+            this.lblMusicName.Name = "lblMusicName";
+            this.lblMusicName.Padding = new System.Windows.Forms.Padding(20, 4, 0, 0);
+            this.lblMusicName.Size = new System.Drawing.Size(302, 21);
+            this.lblMusicName.TabIndex = 0;
+            this.lblMusicName.Text = "label1";
+            // 
+            // btnInput
+            // 
+            this.btnInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnInput.Font = new System.Drawing.Font("微软雅黑", 9.07563F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnInput.Location = new System.Drawing.Point(5, 14);
+            this.btnInput.Name = "btnInput";
+            this.tableLayoutPanel1.SetRowSpan(this.btnInput, 2);
+            this.btnInput.Size = new System.Drawing.Size(106, 48);
+            this.btnInput.TabIndex = 1;
+            this.btnInput.Text = "添加歌曲";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(5, 5);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 3);
+            this.axWindowsMediaPlayer1.TabIndex = 5;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 593);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(438, 639);
+            this.MinimumSize = new System.Drawing.Size(438, 639);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LessMusicPlayer";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblMusicName;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ListBox listBoxMusic;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPrior;
+        private System.Windows.Forms.Button btnNext;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ LessMusicPlayer/LessMusicPlayer/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using WMPLib;
+
+namespace LessMusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        List<string> urlList = new List<string>();
+        double max, min;
+        bool list = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.Multiselect = true;
+            of.Title = "请选择需要播放的文件";
+            of.Filter = "（*.mp3）|*.mp3|（*.wav）|*.wav|（*.flac）|*.flac|（*.ogg）|*.ogg|（所有文件）|*";
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                string[] nameList = of.FileNames;
+                foreach(string url in nameList)
+                {                    
+                    listBoxMusic.Items.Add(Path.GetFileNameWithoutExtension(url));
+                    urlList.Add(url);
+                }
+            };
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBoxMusic.SelectedIndex;
+            if(selectedIndex < 0)
+            {
+                selectedIndex = selectedIndex < 0 ? 0 : selectedIndex; ;
+                //selectedIndex = selectedIndex < 0 ? listBoxMusic.Items.Count - 1 : selectedIndex;
+                listBoxMusic.SelectedIndex = selectedIndex;
+
+                axWindowsMediaPlayer1.URL = urlList[selectedIndex];
+                lblMusicName.Text = listBoxMusic.SelectedItem.ToString();
+            }
+            else
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+            timer1.Enabled = true;
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+            timer1.Enabled = false;
+        }
+
+        private void btnPrior_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBoxMusic.SelectedIndex - 1;
+            selectedIndex = selectedIndex < 0 ? listBoxMusic.Items.Count - 1 : selectedIndex;
+            listBoxMusic.SelectedIndex = selectedIndex;
+            axWindowsMediaPlayer1.URL = urlList[selectedIndex];
+            lblMusicName.Text = listBoxMusic.SelectedItem.ToString();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBoxMusic.SelectedIndex + 1;
+            selectedIndex = selectedIndex == listBoxMusic.Items.Count ? 0 : selectedIndex;
+            listBoxMusic.SelectedIndex = selectedIndex;
+            axWindowsMediaPlayer1.URL = urlList[selectedIndex];
+            lblMusicName.Text = listBoxMusic.SelectedItem.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            max = axWindowsMediaPlayer1.currentMedia.duration;
+            min = axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+
+            trackBar1.Maximum = (int)(max);
+            trackBar1.Value = (int)(min);
+
+            if(axWindowsMediaPlayer1.playState == WMPPlayState.wmppsStopped)
+            {
+                list = true;
+                int seletedIdex = listBoxMusic.SelectedIndex + 1;
+                seletedIdex = seletedIdex == listBoxMusic.Items.Count ? 0 : seletedIdex;
+                axWindowsMediaPlayer1.URL = urlList[seletedIdex];
+                listBoxMusic.SelectedIndex = seletedIdex;
+                lblMusicName.Text = listBoxMusic.SelectedItem.ToString();
+                trackBar1.Value = 0;
+                timer1.Enabled = true;
+                
+            }
+            list = false;
+        }
+
+        private void trackBar1_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = false;
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void trackBar1_MouseUp(object sender, MouseEventArgs e)
+        {
+            double doValue = trackBar1.Value;
+            axWindowsMediaPlayer1.Ctlcontrols.currentPosition = doValue;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            timer1.Enabled = true;
+        }
+
+        private void listBoxMusic_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = listBoxMusic.SelectedIndex;
+            axWindowsMediaPlayer1.URL = urlList[selectedIndex];
+            lblMusicName.Text = listBoxMusic.SelectedItem.ToString();
+            if(list == true)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+            if(list == false)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.stop();
+            }
+            
+        }
+    }
+}
